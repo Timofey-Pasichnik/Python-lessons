325286afb973815d7e60bd5ec77ca6a281baa6c7
Timofey-Pasichnik <timmofonnius@gmail.com>
2020-05-19 19:31:13 +0300

    22

---
+++ Lessons/Lesson-16-modules.py
@@ -0,0 +1,8 @@
+import Module for lesson-16
+x = __import__('Module for lesson-16')
+
+
+aaaa()
+bbbb()
+cccc()
+dddd()
---
+++ Lessons/Module for lesson-16.py
@@ -0,0 +1,14 @@
+def aaaa():
+    print('AAAA')
+
+
+def bbbb():
+    print('BBBB')
+
+
+def cccc():
+    print('CCCC')
+
+
+def dddd():
+    print('DDDD')
